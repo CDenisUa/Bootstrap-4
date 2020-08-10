6a561ad3adab31dd2e09f6406fd1464e2f0e647a
CDenis <Denclub2013@gmail.com>
2020-08-10 16:47:53 +0300

    Tab version finished v.0.8.0

--- Web Development/js/main.js
+++ Web Development/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    /*Portfolio*/
+
     $('.button[filter]').click(function () {
         if ($(this).attr('filter') == 'all') {
             if ($(this).attr('val') == 'off') {
@@ -21,12 +23,73 @@ $(document).ready(function () {
             }
     })
 
+    /*Slider*/
+
     $('.multiple-items').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: false,
         dots: true,
-        dotsClass: 'dots-style'
+        dotsClass: 'dots-style',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
+    });
+
+    /*Menu*/
+
+    $('ul.menu a[href^="#"').click(function () {
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('ul.menu a[href^="#"').css({
+            'color': '#212121'
+        });
+        $(this).css({
+            'color': '#004bee'
+        });
+        return false;
+    });
+
+    /*Menu show*/
+    console.log
+    $('.menu-icon').click(function () {
+        $('nav').slideToggle(500);
+        $('ul.menu').css({
+            'display': 'flex',
+            'flex-direction': 'column'
+        });
+
+        if ($('.menu-icon > i').hasClass('showMenu') == true) {
+            $('.menu-icon').html('<i class="fas fa-times showMenu"></i>');
+            $('.menu-icon > i').removeClass('showMenu');
+        } else {
+            $('.menu-icon').html('<i class="fas fa-bars showMenu"></i>');
+        }
+    });
+
+    /*Button to top*/
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
     });
 });
